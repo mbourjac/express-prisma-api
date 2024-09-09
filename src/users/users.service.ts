e872e1a6170add8fa601b01f71ec3760d327314d
Michaël Bourjac <michael.bourjac@gmail.com>
2024-09-09 08:05:31 +0200

    feat: implement user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,16 @@
+import bcrypt from 'bcrypt';
 import type { UsersRepository } from './users.repository.js';
 import type { CreateUser } from './users.types.js';
 
 export class UsersService {
   constructor(private userRepository: UsersRepository) {}
 
-  async createUser(data: CreateUser) {}
+  async createUser(data: CreateUser) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.userRepository.createUser({
+      ...data,
+      password: hashedPassword,
+    });
+  }
 }
